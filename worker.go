ead18b5a411b6dad055f5ca3d97d29ae7bb1869a
Joshua Smith <joshua.smith@rakuten.com>
2018-01-18 14:49:03 -0700

    Add some helper methods

--- worker.go
+++ worker.go
@@ -1,6 +1,9 @@
 package task
 
-import "context"
+import (
+	"context"
+	"fmt"
+)
 
 type Worker interface {
 	// DoTask will return a Result and msg string.
@@ -10,3 +13,23 @@ type Worker interface {
 // MakeWorker is called by the Launcher to
 // generate a new worker for a new task.
 type MakeWorker func(info string) Worker
+
+// IsDone is a helper function that determines if ctx has been canceled
+func IsDone(ctx context.Context) bool {
+	select {
+	case <-ctx.Done():
+		return true
+	default:
+		return false
+	}
+}
+
+// Interrupted is a helper function that can be called when DoTask was interrupted
+func Interrupted() (Result, string) {
+	return ErrResult, "task interrupted"
+}
+
+// Completed is a helper function that can be called when DoTask has completed
+func Completed(format string, a ...interface{}) (Result, string) {
+	return CompleteResult, fmt.Sprintf(format, a)
+}
