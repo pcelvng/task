f3cb262dce7e642f52f4f8a3b7e471f6af10d6f8
Ryan Ball <rball@balldevelopment.com>
2018-02-19 17:45:01 -0700

    misc updates

--- bus/bus.go
+++ bus/bus.go
@@ -1,6 +1,21 @@
 package bus
 
 type Consumer interface {
+	// Msg returns the bus msg bytes. If msg is
+	// known to be the last then done should be true. msg may be
+	// nil and done can be true. err should never be
+	// io.EOF.
+	//
+	// Once the last message has been received, subsequent
+	// calls to Msg should not block and always return
+	// msg == nil (or len == 0), done == true and err == nil.
+	//
+	// A call to Msg should block until either a msg
+	// is received or Stop has been called.
+	//
+	// Once Stop has been called subsequent calls to Msg
+	// should not block and immediately return with
+	// msg == nil (or len == 0), done == true and err == nil.
 	Msg() (msg []byte, done bool, err error)
 	Stop() error
 }
